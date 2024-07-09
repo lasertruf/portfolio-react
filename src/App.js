6152e84efd6fa85be445d6b899b6a930a5ab9f30
lasertruf <jayantwaghmare9@gmail.com>
2024-07-09 23:49:00 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import ProjectsComponent from "./components/projects/projects";
+import "./styles/buttons.scss";
+
+import photo from "./assets/jayant-insta.png"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="m-8">
+      <div className="h-12"></div>
+      <div className="flex w-auto items-start">
+
+        <div className="flex-1 text-3xl  text-gray-300 p-6 px-24 py-24">
+          I'm{" "}
+          <span className="text-3xl font-bold" style={{ color: "#ffc86b" }}>
+            Jayant Waghmare
+
+          </span>
+          <div className="text-xl">
+            Full-stack developer, technology enthusiast
+          </div>
+          <div className="mt-2">
+            <button className="btn-pri">Contact me</button>
+          </div>
+        </div>
+
+        <div className="text-white w-1/5">
+        <img  src={photo} />
+        
+        </div>
+
+        <div className="w-1/5 ml-24 mt-10">
+
+              <div className="flex-col text-white">
+                <div>LinkedIn</div>
+                <div className="mt-4">GitHub</div>
+              </div>
+        </div>
+
+      </div>
+
+      <ProjectsComponent/>
+     
     </div>
   );
 }
---
+++ src/components/projects/projects.jsx
@@ -0,0 +1,52 @@
+
+function ProjectsComponent() {
+  return (
+    <div className="mt-10 w-3/4">
+      <div className="flex  items-center gap-4 text-3xl text-primary font-bold">
+        <div className="h-0.5 bg-primary w-8"> </div>
+        About me
+        <div className="h-0.5 bg-primary flex-auto"> </div>
+      </div>
+
+      <div className="text-white text-lg mx-8 p-4">
+        Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
+        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
+        maiores cumque nulla deserunt inventore dignissimos!
+      </div>
+
+      <div className="flex  items-center gap-4 text-3xl text-primary font-bold">
+        <div className="h-0.5 bg-primary w-8"> </div>
+        Projects
+        <div className="h-0.5 bg-primary flex-auto"> </div>
+      </div>
+
+
+      <div className="text-white text-lg mx-8 p-4">
+        Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
+        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
+        maiores cumque nulla deserunt inventore dignissimos!
+      </div>
+
+
+
+      <div className="flex  items-center gap-4 text-3xl text-primary font-bold">
+        <div className="h-0.5 bg-primary w-8"> </div>
+        Tech stack
+        <div className="h-0.5 bg-primary flex-auto"> </div>
+      </div>
+
+      <div className="text-white text-lg mx-8 p-4">
+        Lorem, ipsum dolor sit amet
+        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
+        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
+        maiores cumque nulla deserunt inventore dignissimos!
+      </div>
+
+
+    </div>
+  );
+}
+
+export default ProjectsComponent;
