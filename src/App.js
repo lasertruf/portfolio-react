0d49c98a7add9f57dc164944ee2ba377dff50310
lasertruf <jayantwaghmare9@gmail.com>
2024-07-13 21:11:07 +0530

    setup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import "./App.css";
 import ProjectsComponent from "./components/projects/projects";
 import "./styles/buttons.scss";
+import { SocialIcon } from 'react-social-icons/component'
+import 'react-social-icons/linkedin'
+import 'react-social-icons/github'
 
 import photo from "./assets/jayant-insta.png"
 
@@ -10,7 +13,7 @@ function App() {
       <div className="h-12"></div>
       <div className="flex w-auto items-start">
 
-        <div className="flex-1 text-3xl  text-gray-300 p-6 px-24 py-24">
+        <div className="flex-1 text-3xl  text-gray-300 p-6 px-12 pt-24">
           I'm{" "}
           <span className="text-3xl font-bold" style={{ color: "#ffc86b" }}>
             Jayant Waghmare
@@ -19,24 +22,29 @@ function App() {
           <div className="text-xl">
             Full-stack developer, technology enthusiast
           </div>
-          <div className="mt-2">
+          {/* <div className="mt-2">
             <button className="btn-pri">Contact me</button>
-          </div>
+          </div> */}
         </div>
 
         <div className="text-white w-1/5">
-        <img  src={photo} />
+        {/* <img  src={photo} /> */}
         
         </div>
 
-        <div className="w-1/5 ml-24 mt-10">
+        {/* <div className="w-1/5 ml-24 mt-10">
 
               <div className="flex-col text-white">
                 <div>LinkedIn</div>
                 <div className="mt-4">GitHub</div>
               </div>
-        </div>
+        </div> */}
 
+      </div>
+
+      <div className="px-11 gap-2 flex">
+      <SocialIcon  target="_blank" style={{width:"40px", height:"40px"}}  bgColor="#2a2828" url="https://github.com/lasertruf" />
+      <SocialIcon target="_blank"  style={{width:"40px", height:"40px"}} bgColor="#2a2828" url="https://www.linkedin.com/in/jayant-waghmare-124294169/" />
       </div>
 
       <ProjectsComponent/>
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import "./projects.scss"
 
 function ProjectsComponent() {
   return (
@@ -9,10 +11,13 @@ function ProjectsComponent() {
       </div>
 
       <div className="text-white text-lg mx-8 p-4">
-        Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
-        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
-        maiores cumque nulla deserunt inventore dignissimos!
+        I'm a passionate and dedicated software developer with over 3 years of
+        professional experience. My expertise spans across a variety of modern
+        technologies including Angular, React, Node.js, and Java. I thrive on
+        creating robust and scalable web applications, and I am committed to
+        continuous learning and staying updated with the latest industry trends.
+        My strong problem-solving skills and collaborative mindset enable me to
+        deliver high-quality solutions that meet both user and business needs.
       </div>
 
       <div className="flex  items-center gap-4 text-3xl text-primary font-bold">
@@ -21,15 +26,12 @@ function ProjectsComponent() {
         <div className="h-0.5 bg-primary flex-auto"> </div>
       </div>
 
-
       <div className="text-white text-lg mx-8 p-4">
-        Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
-        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
-        maiores cumque nulla deserunt inventore dignissimos!
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore natus
+        ad possimus ex odit eos quidem commodi obcaecati ut. Maxime temporibus
+        neque expedita delectus maiores cumque nulla deserunt inventore
+        dignissimos!
       </div>
-
-
 
       <div className="flex  items-center gap-4 text-3xl text-primary font-bold">
         <div className="h-0.5 bg-primary w-8"> </div>
@@ -37,14 +39,29 @@ function ProjectsComponent() {
         <div className="h-0.5 bg-primary flex-auto"> </div>
       </div>
 
-      <div className="text-white text-lg mx-8 p-4">
-        Lorem, ipsum dolor sit amet
-        consectetur adipisicing elit. Tempore natus ad possimus ex odit eos
-        quidem commodi obcaecati ut. Maxime temporibus neque expedita delectus
-        maiores cumque nulla deserunt inventore dignissimos!
+      <div className="flex justify-start items-star mx-4">
+        <div className="progress-bar-container">
+          <div className="progress-bar" data-label="Angular">
+            <div className="progress-bar-inner" style={{width:"95%"}}></div>
+            <div className="progress-bar-label">Angular</div>
+          </div>
+
+          <div className="progress-bar" data-label="Angular">
+            <div className="progress-bar-inner" style={{width:"90%"}}></div>
+            <div className="progress-bar-label">React</div>
+          </div>
+
+          <div className="progress-bar" data-label="Angular">
+            <div className="progress-bar-inner" style={{width:"85%"}}></div>
+            <div className="progress-bar-label">Node JS</div>
+          </div>
+
+          <div className="progress-bar" data-label="Angular">
+            <div className="progress-bar-inner" style={{width:"80%"}}></div>
+            <div className="progress-bar-label">JAVA</div>
+          </div>
+        </div>
       </div>
-
-
     </div>
   );
 }
